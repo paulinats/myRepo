8ba643429b0df21fe8c56bff20aa756c0f643d7a
paulinats <71715041+paulinats@users.noreply.github.com>
2021-02-16 00:25:53 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./styles.css";
 
 export default function App() {
   return (
+    <div className="App">
       <h2>Start editing to see some magic happen!</h2>
-    </div>
+    </div><div>
   );
 }
