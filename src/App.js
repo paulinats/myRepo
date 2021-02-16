e97e92565acaa73539a2e9d686706ed021a918e8
paulinats <71715041+paulinats@users.noreply.github.com>
2021-02-16 08:23:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App"><div>
+    <h1></h1?
       <h2>Start editing to see some magic happen!</h2>
-    </div><div>
+    </div></div>
   );
 }
