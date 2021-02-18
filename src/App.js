21e742130018fb9d6ac8e27d8ad0d7f1b141f160
paulinats <71715041+paulinats@users.noreply.github.com>
2021-02-18 15:32:28 +0200

    hghgjh

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h2>Start editing to see some magic happen!</h2>sfsdfsf
+      <h2>Start editing to see some magic happen!</h2>kasdkj
       <h2>Start editing to see some magic happen!</h2>sjhrjewkjsfksjkjts
+<<<<<<< Codesandbox
+    </div><div></div>mnnnj<div></div>
+=======
     </div><>mnnnj
+>>>>>>> GitHub
     <div></div>
   );
 }
